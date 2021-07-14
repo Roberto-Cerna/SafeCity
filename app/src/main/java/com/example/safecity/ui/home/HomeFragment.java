c778d26e70d55895a297a68c46eced564bc52296
Roberto Cerna <roberto.cerna.e@uni.pe>
2021-07-14 11:10:07 -0500

    minor changes

--- app/src/main/java/com/example/safecity/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/safecity/ui/home/HomeFragment.java
@@ -117,6 +117,7 @@ public class HomeFragment extends Fragment implements LocationListener {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Diga ayuda");
                 if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
                     startActivityForResult(intent, 10);
                 }else{
